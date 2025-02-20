453ebc6fcf21bcb4aef23f1a923277535484a1f5
Hole-113 <122269835+Hole-113@users.noreply.github.com>
2025-02-20 09:53:30 +0530

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<tc/tc_startup.h>
+#include<tcinit/tcinit.h>
+#include<tccore/custom.h>
+#include<tccore/method.h>
+#include<tccore/aom_prop.h>
+#include<fclasses/tc_string.h>
+
+
+// syntax for DLL code
+#define DLLAPI _declspec(dllexport)
+#define PLM_error (EMH_USER_error_base +2)
+
+extern DLLAPI int Custom_Exit_sep_register_callbacks();
+
+extern DLLAPI int plm_execute_callbacks1(int * decision,va_list argc);
+extern DLLAPI int plm_execute_callbacks2(int* decision, va_list argc);
+extern DLLAPI int check_release_status(METHOD_message_t* msg, va_list args);
