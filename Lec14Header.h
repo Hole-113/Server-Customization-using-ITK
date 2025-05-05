624ae3c1ddf3ea29fc300cbe31bfbb5deb90859a
Hole-113 <122269835+Hole-113@users.noreply.github.com>
2025-05-05 13:01:39 +0530

    Update Lec14Header.h

--- Lec14Header.h
+++ Lec14Header.h
@@ -16,6 +16,12 @@
 
 // syntax for DLL code
 #define DLLAPI _declspec(dllexport)
+/*  __declspec = "declare specifier" 
+(dllexport) = "DLL export"
+Stands for Dynamic-Link Library Export.
+It tells the compiler to export the symbol (function, variable, class) from a DLL so that other applications or modules can use it.
+*/
+
 #define PLM_error (EMH_USER_error_base +2)
 
 extern DLLAPI int Custom_Exit_sep_register_callbacks();
